da10b155f802e3fff6b2b08c10e7fd0adad25bd9
Jack Ellis <Jack@Administrators-MacBook-Pro.local>
2017-02-22 14:21:50 +0000

    jpex-folder 2.0

--- spec/jpex-folder/smartfolder.spec.js
+++ spec/jpex-folder/smartfolder.spec.js
@@ -1,49 +1,49 @@
-/* globals describe, expect, it, beforeEach, afterEach ,spyOn*/
-var grequire = require('../../jpex-folder/grequire');
-
 describe('Base Class - Dependency Injection', function(){
-  var Base, First;
-  
+  var Jpex, plugin, First;
+
   beforeEach(function(){
-    grequire('.')();
-    Base = grequire('node_modules/jpex');
-    First = Base.extend();
+    Object.keys(require.cache).forEach(key => delete require.cache[key]);
+
+    Jpex = require('jpex');
+    plugin = require('../../src');
+    Jpex.use(plugin);
+    First = Jpex.extend();
   });
 
   describe('Registration', function(){
     describe('Smart Folder', function(){
       it('should register a file from a folder', function(done){
-        First.Register.Folder('../spec/jpex-folder/smart', {pattern : 'constant.*'});
-        
-        expect(First._factories.constant).toBeDefined();
-        
+        First.register.folder('./spec/jpex-folder/smart', {pattern : 'constant.*'});
+
+        expect(First.$$factories.constant).toBeDefined();
+
         var Second  = First.extend(function(constant){
           expect(constant).toBeDefined();
           expect(constant.test).toBe('i am a constant');
           done();
         });
-        
-        new Second();
+
+        Second();
       });
       it('should register an index.js file', function(done){
-        First.Register.Folder('../spec/jpex-folder/smart', {pattern : 'folder/*.*'});
-        expect(First._factories.folder).toBeDefined();
-        
+        First.register.folder('./spec/jpex-folder/smart', {pattern : 'folder/*.*'});
+        expect(First.$$factories.folder).toBeDefined();
+
         var Second  = First.extend(function(folder){
           expect(folder).toBeDefined();
           expect(folder).toBe('i am an index.js file');
           done();
         });
-        
-        new Second();
+
+        Second();
       });
       it('should load multiple files from within multiple folders', function(done){
-        First.Register.Folder('../spec/jpex-folder/smart', {pattern : 'folders/**/*.*'});
-        expect(First._factories.foldersFirst).toBeDefined();
-        expect(First._factories.foldersSecond).toBeDefined();
-        expect(First._factories.foldersSubFirst).toBeDefined();
-        expect(First._factories.foldersSubSecond).toBeDefined();
-        
+        First.register.folder('./spec/jpex-folder/smart', {pattern : 'folders/**/*.*'});
+        expect(First.$$factories.foldersFirst).toBeDefined();
+        expect(First.$$factories.foldersSecond).toBeDefined();
+        expect(First.$$factories.foldersSubFirst).toBeDefined();
+        expect(First.$$factories.foldersSubSecond).toBeDefined();
+
         var Second = First.extend(function(foldersFirst, foldersSecond, foldersSubFirst, foldersSubSecond){
           expect(foldersFirst).toBe('first');
           expect(foldersSecond).toBe('second');
@@ -51,30 +51,30 @@ describe('Base Class - Dependency Injection', function(){
           expect(foldersSubSecond).toBe('sub second');
           done();
         });
-        
-        new Second();
+
+        Second();
       });
       it('should accept a custom name transformer', function(){
-        var expected = ['index', 'first', 'second', 'first', 'second', 'constant', 'factory', 
+        var expected = ['index', 'first', 'second', 'first', 'second', 'constant', 'factory',
                         'service', 'enums', 'iinterface', 'jpexservice'];
-        
-        First.Register.Folder('../spec/jpex-folder/smart', {
+
+        First.register.folder('./spec/jpex-folder/smart', {
           pattern : '**/*.{js,json}',
           transform : function(file, folders, ext){
             expect(typeof file).toBe('string');
             expect(Array.isArray(folders)).toBe(true);
             expect(ext[0]).toBe('.');
-            
+
             var i = expected.indexOf(file);
             expect(i).toBeGreaterThan(-1);
             expected.splice(i, 1);
           }
         });
-        
+
         expect(expected.length).toBe(0);
       });
       it('should exclude any null transformer results', function(){
-        First.Register.Folder('../spec/jpex-folder/smart', {
+        First.register.folder('./spec/jpex-folder/smart', {
           pattern : '**/*.*',
           transform : function(file){
             if (file === 'first' || file === 'constant'){
@@ -84,9 +84,9 @@ describe('Base Class - Dependency Injection', function(){
             }
           }
         });
-        expect(First._factories.first).toBeDefined();
-        expect(First._factories.constant).toBeDefined();
-        expect(Object.keys(First._factories).length).toBe(2);
+        expect(First.$$factories.first).toBeDefined();
+        expect(First.$$factories.constant).toBeDefined();
+        expect(Object.keys(First.$$factories).length).toBe(2);
       });
       it('should accept a custom register function', function(){
         var opt = {
@@ -94,25 +94,25 @@ describe('Base Class - Dependency Injection', function(){
           register : function(){}
         };
         spyOn(opt, 'register');
-        First.Register.Folder('../spec/jpex-folder/smart', opt);
+        First.register.folder('./spec/jpex-folder/smart', opt);
         expect(opt.register).toHaveBeenCalled();
       });
-      
+
       it('should register a factory', function(done){
-        First.Register.Folder('../spec/jpex-folder/smart', {type : 'factory', pattern : 'factory.js'});
-        
+        First.register.folder('./spec/jpex-folder/smart', {type : 'factory', pattern : 'factory.js'});
+
         var Second = First.extend(function(factory){
           expect(factory).toBe('i am a factory');
           done();
         });
-        
-        new Second();
+
+        Second();
       });
       it('should not register an invalid factory', function(){
         var err;
-        
+
         try{
-          First.Register.Folder('../spec/jpex-folder/smart', {type : 'factory', pattern : 'constant.json'});
+          First.register.folder('./spec/jpex-folder/smart', {type : 'factory', pattern : 'constant.json'});
         }
         catch(e){
           err = e;
@@ -122,20 +122,20 @@ describe('Base Class - Dependency Injection', function(){
         }
       });
       it('should register a service', function(done){
-        First.Register.Folder('../spec/jpex-folder/smart', {type : 'service', pattern : 'service.js'});
-        
+        First.register.folder('./spec/jpex-folder/smart', {type : 'service', pattern : 'service.js'});
+
         var Second = First.extend(function(service){
           expect(service.test).toBe('i am a service');
           done();
         });
-        
-        new Second();
+
+        Second();
       });
       it('should not register an invalid service', function(){
         var err;
-        
+
         try{
-          First.Register.Folder('../spec/jpex-folder/smart', {type : 'service', pattern : 'constant.json'});
+          First.register.folder('./spec/jpex-folder/smart', {type : 'service', pattern : 'constant.json'});
         }
         catch(e){
           err = e;
@@ -145,77 +145,39 @@ describe('Base Class - Dependency Injection', function(){
         }
       });
       it('should register a constant', function(done){
-        First.Register.Folder('../spec/jpex-folder/smart', {type : 'constant', pattern : 'constant.json'});
-        
+        First.register.folder('./spec/jpex-folder/smart', {type : 'constant', pattern : 'constant.json'});
+
         var Second = First.extend(function(constant){
           expect(constant.test).toBe('i am a constant');
           done();
         });
-        
-        new Second();
+
+        Second();
       });
-      it('should register an enum', function(done){
-        First.Register.Folder('../spec/jpex-folder/smart', {type : 'enum', pattern : 'enums.json'});
-        
-        var Second = First.extend(function(enums){
-          expect(enums.test).toBeDefined();
-          done();
-        });
-        
-        new Second();
-      });
-      
-      it('should register an interface', function(done){
-        First.Register.Folder('../spec/jpex-folder/smart', {
-          type : 'interface',
-          pattern : 'iinterface.js'
-        });
-        First.Register.Folder('../spec/jpex-folder/smart', {
-          pattern : 'jpexservice.js'
-        });
-        
-        var Second = First.extend(function(iinterface){
-          expect(iinterface).toBeDefined();
-          expect(iinterface.test).toBe('jpex service');
-          done();
-        });
-        
-        new Second();
-      });
-      
+
       it('should determine how to register a file automatically', function(){
-        First.Register.Folder('../spec/jpex-folder/smart', {pattern : '{constant,factory}.*'});
+        First.register.folder('./spec/jpex-folder/smart', {pattern : '{constant,factory}.*'});
         var Second = First.extend(function(constant, factory){
           expect(constant.test).toBe('i am a constant');
           expect(factory).toBe('i am a factory');
         });
+        Second();
       });
       it('should add a prefix and suffix to the dependency', function(){
-        First.Register.Folder('../spec/jpex-folder/smart', {prefix : 'pre', suffix : 'post'});
-        
-        expect(First._factories.preFactoryPost).toBeDefined();
-        expect(First._factories.preFoldersSubFirstPost).toBeDefined();
+        First.register.folder('./spec/jpex-folder/smart', {prefix : 'pre', suffix : 'post'});
+
+        expect(First.$$factories.preFactoryPost).toBeDefined();
+        expect(First.$$factories.preFoldersSubFirstPost).toBeDefined();
       });
-      
+
       it('should not prefix the folder name', function(){
-        First.Register.Folder('../spec/jpex-folder/smart', {
+        First.register.folder('./spec/jpex-folder/smart', {
           prefixFolder : false,
           pattern : '**/first.js'
         });
-        
-        expect(First._factories.first).toBeDefined();
-        expect(Object.keys(First._factories).length).toBe(1);
-      });
-      
-      it('should attach an interface to the factory', function(){
-        First.Register.Folder('../spec/jpex-folder/smart', {
-          type : 'constant', 
-          pattern : 'constant.json', 
-          interface : 'iinterface'
-        });
-        
-        expect(First._factories.constant.interface).toBeDefined();
-        expect(First._factories.constant.interface).toEqual(['iinterface']);
+
+        expect(First.$$factories.first).toBeDefined();
+        expect(Object.keys(First.$$factories).length).toBe(1);
       });
     });
   });
---
+++ src/index.js
@@ -0,0 +1,154 @@
+const glob = require('glob');
+const path = require('path');
+
+module.exports = {
+    install : function ({Jpex, on}) {
+        Jpex.register.folder = folder.bind(Jpex, {});
+
+        on('factories', function ({register, options, Class}) {
+            register('folder', folder.bind(Class, options));
+        });
+        on('privateProperties', function ({options, apply}) {
+            apply({
+                $$dependencies : {
+                    get : function () {
+                        return (options.dependencies || []).slice();
+                    }
+                }
+            });
+        });
+    }
+};
+
+function folder(jpexConfig, target, options) {
+    options = Object.assign({
+        type : 'auto',
+        lifecycle : jpexConfig.defaultLifecycle,
+        prefix : '',
+        suffix : '',
+        prefixFolder : true,
+        pattern : '**/*.js',
+        transform : transform,
+        register : register
+    }, options);
+
+    const fullPath = path.isAbsolute(target) ? target : path.resolve(target);
+
+  // Retrieve all files that match the provided pattern
+    glob
+    .sync(options.pattern, {cwd : fullPath})
+    .map(filename => {
+        const info = path.parse(filename);
+        const folders = info.dir ? info.dir.split('/') : [];
+        const ext = info.ext;
+        const base = info.name;
+        const name = options.transform.call(this, base, folders, ext, options);
+
+        return {
+            path : [target, filename].join('/'),
+            file : base,
+            name : name
+        };
+    })
+    .filter(function (file) {
+      // Remove empty names
+        return !!file.name;
+    })
+    .forEach(file => {
+        const content = require(path.resolve(file.path));
+        options.register.call(this, file.name, content, options);
+    });
+
+    return this;
+}
+
+function transform(file, folders, ext, options) {
+    const result = [];
+
+    if (options.prefix){
+        result.push(options.prefix);
+    }
+    if (options.prefixFolder){
+        result.push(folders);
+    }
+    if (file !== 'index'){
+        result.push(file);
+    }
+    if (options.suffix){
+        result.push(options.suffix);
+    }
+
+    const name = result.map(n => toPascal(n)).join('');
+    return name.charAt(0).toLowerCase() + name.substr(1);
+}
+
+function register(name, data, options) {
+    let f;
+    switch(options.type.toLowerCase()){
+    case 'factory':
+        assertFunction(data, 'Factory');
+        f = this.register.factory(name, data.dependencies, data);
+        break;
+
+    case 'service':
+        assertFunction(data, 'Service');
+        f = this.register.service(name, data.dependencies || data.$$dependencies, data);
+        break;
+
+    case 'constant':
+        f = this.register.constant(name, data);
+        return;
+
+    case 'auto':
+        if (typeof data === 'function'){
+            if (data.extend && data.register && data.register.service){
+                f = this.register.service(name, data.dependencies || data.$$dependencies, data);
+            }else{
+                f = this.register.factory(name, data.dependencies, data);
+            }
+        }else{
+            f = this.register.constant(name, data);
+            return;
+        }
+        break;
+    }
+
+    if (!f){
+        return;
+    }
+
+    if (data && data.lifecycle){
+        switch(typeof data.lifecycle){
+        case 'string':
+            if (f.lifecycle[data.lifecycle]){
+                f.lifecycle[data.lifecycle]();
+            }
+            break;
+        case 'number':
+            this.$$factories[name].lifecycle = data.lifecycle;
+            break;
+        }
+    }else if (options.lifecycle){
+        switch(typeof options.defaultLifecycle){
+        case 'string':
+            if (f.lifecycle[options.lifecycle]){
+                f.lifecycle[options.lifecycle]();
+            }
+            break;
+        case 'number':
+            this.$$factories[name].lifecycle = options.lifecycle;
+            break;
+        }
+    }
+}
+
+function toPascal(arr) {
+    return [].concat(arr).map(folder => folder.charAt(0).toUpperCase() + folder.substring(1).toLowerCase()).join('');
+}
+
+function assertFunction(obj, expected) {
+    const t = typeof obj;
+    if (t !== 'function'){
+        throw new Error(expected + ' type expected but got ' + t);
+    }
+}
